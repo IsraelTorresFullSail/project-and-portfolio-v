2054ea19ffe00dc906bcf87011dc1ed143e0aedc
Israel Torres <israel.a.torres01@gmail.com>
2020-01-28 22:34:49 -0500

    Updates

---
+++ precache-manifest.f7b93b4744fe21754c90d2d3c625a969.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be4a027f516d3c409b290f20b191d84",
+    "url": "/project-and-portfolio-v/index.html"
+  },
+  {
+    "revision": "1d7d37d7683d441f8556",
+    "url": "/project-and-portfolio-v/static/css/main.b7e4f70b.chunk.css"
+  },
+  {
+    "revision": "292d8405b7e7abae24ec",
+    "url": "/project-and-portfolio-v/static/js/2.73d2dd0b.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/project-and-portfolio-v/static/js/2.73d2dd0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d7d37d7683d441f8556",
+    "url": "/project-and-portfolio-v/static/js/main.a64a30a0.chunk.js"
+  },
+  {
+    "revision": "c5c3300ed7d39616019e",
+    "url": "/project-and-portfolio-v/static/js/runtime-main.4ca1b7ba.js"
+  },
+  {
+    "revision": "7374c34f11fc1f2eee3f976d7d1686b6",
+    "url": "/project-and-portfolio-v/static/media/bg-instructions.7374c34f.jpg"
+  },
+  {
+    "revision": "c4f3114afda1e1abf2393b77cd59af5c",
+    "url": "/project-and-portfolio-v/static/media/bg-profile.c4f3114a.jpg"
+  },
+  {
+    "revision": "d74384de0fb3c2f193fdf86c6d6f26d5",
+    "url": "/project-and-portfolio-v/static/media/goal-bg.d74384de.jpg"
+  },
+  {
+    "revision": "c052027a924771179ff4a68a7033303f",
+    "url": "/project-and-portfolio-v/static/media/logo.c052027a.png"
+  }
+]);
